e80a9626878949e8a0f9c1dca088bc331ac3a6df
Chris J. Karr <chris@audacious-software.com>
2026-08-04 17:29:10 -0500

    Code cleanup

--- simple_messaging_api.py
+++ simple_messaging_api.py
@@ -1,4 +1,4 @@
-# pylint: disable=line-too-long, no-member
+# pylint: disable=line-too-long, no-member, import-error
 
 import calendar
 import json
@@ -10,7 +10,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from simple_messaging.models import IncomingMessage, OutgoingMessage
+from simple_messaging.models import IncomingMessage, OutgoingMessage 
 
 from .models import ResearchParticipant
 
